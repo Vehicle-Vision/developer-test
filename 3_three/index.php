06415056acc701fee8010d872ec197ba818120ac
mr.shahib.ss@gmail.com <mr.shahib.ss@gmail.com>
2022-02-07 21:49:05 +0000

    I tried what i could but backend is not my strong point

--- 3_three/index.php
+++ 3_three/index.php
@@ -1,7 +1,36 @@
 <?php
 
-/*
-  # EXERCISE THREE
-*/
+function curl_get_file_size( $url ) {
+  $result = -1;
 
+  $curl = curl_init( $url );
+
+  curl_setopt( $curl, CURLOPT_NOBODY, true );
+  curl_setopt( $curl, CURLOPT_HEADER, true );
+  curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+  curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
+  curl_setopt( $curl, CURLOPT_USERAGENT, get_user_agent_string() );
+
+  $data = curl_exec( $curl );
+  curl_close( $curl );
+
+  if( $data ) {
+    $content_length = "unknown";
+    $status = "unknown";
+
+    if( preg_match( "/^HTTP\/1\.[01] (\d\d\d)/", $data, $matches ) ) {
+      $status = (int)$matches[1];
+    }
+
+    if( preg_match( "/Content-Length: (\d+)/", $data, $matches ) ) {
+      $content_length = (int)$matches[1];
+    }
+
+    if( $status == 200 || ($status > 300 && $status <= 308) ) {
+      $result = $content_length;
+    }
+  }
+
+  return $result;
+}
 ?>
