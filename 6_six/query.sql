4f38c858d9dc37790bd1009d4f03a2529835860a
Mohan Dharavath <dharavath.mohan47@gmail.com>
2022-02-08 02:19:13 +0000

    SQL mistakes are assumed and rectified

--- 6_six/query.sql
+++ 6_six/query.sql
@@ -19,3 +19,8 @@ WHERE `type` = 'job' AND J1.`field_centre_reference_nid` = 1000
 AND R2.field_follow_up_in_value NOT NULL AND R2.field_follow_up_in_value != 0
 AND J3.`field_test_job_value` != 1 
 AND DATE_ADD(UNIXTIME(A.`created`), INTERVAL R2.field_follow_up_in_value - 7 DAY) < DATE_NOW()
+
+ 
+At line 17 "  INTERVAL R2.field_follow_up_in_value - 7 DAY " --- syntax should be written as INTERVAL (R2.field_follow_up_in_value - 7) DAY
+At line 21 " R2.field_follow_up_in_value NOT NULL " --- syntax should be IS NOT NULL
+At line 23 " INTERVAL R2.field_follow_up_in_value - 7 DAY "  --- syntax should be written as INTERVAL (R2.field_follow_up_in_value - 7) DAY
